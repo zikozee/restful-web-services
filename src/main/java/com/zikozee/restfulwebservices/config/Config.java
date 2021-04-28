a25647a734d84500f6763ee39abbc074772c2bff
ezekiel.eromosei <ezekiel.eromosei@longbridgetech.com>
2021-04-28 15:13:34 +0100

    using auto conig to setup i18n

--- src/main/java/com/zikozee/restfulwebservices/config/Config.java
+++ src/main/java/com/zikozee/restfulwebservices/config/Config.java
@@ -2,9 +2,8 @@ package com.zikozee.restfulwebservices.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
-import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
 
 import java.util.Locale;
 
@@ -17,15 +16,15 @@ public class Config {
 
     @Bean
     public LocaleResolver localeResolver() {
-        SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+        AcceptHeaderLocaleResolver localeResolver = new AcceptHeaderLocaleResolver();
         localeResolver.setDefaultLocale(Locale.US);
         return localeResolver;
     }
 
-    @Bean
-    public ResourceBundleMessageSource bundleMessageSourceMessageSource() {
-        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
-        messageSource.setBasename("messages");
-        return messageSource;
-    }
+//    @Bean
+//    public ResourceBundleMessageSource bundleMessageSourceMessageSource() {
+//        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+//        messageSource.setBasename("messages");
+//        return messageSource;
+//    }
 }
--- src/main/java/com/zikozee/restfulwebservices/controller/HelloWorldController.java
+++ src/main/java/com/zikozee/restfulwebservices/controller/HelloWorldController.java
@@ -3,12 +3,10 @@ package com.zikozee.restfulwebservices.controller;
 import com.zikozee.restfulwebservices.domain.helloworld.HelloWorldBean;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Locale;
 
 /**
  * @author : zikoz
@@ -32,7 +30,7 @@ public class HelloWorldController {
     }
 
     @GetMapping(path = "hello-world-internationalized")
-    public String helloWorldInternationalized(@RequestHeader(name = "Accept-Language", required = false) Locale locale){
-        return messageSource.getMessage("good.morning.message", null, locale);
+    public String helloWorldInternationalized(){
+        return messageSource.getMessage("good.morning.message", null, LocaleContextHolder.getLocale());
     }
 }
